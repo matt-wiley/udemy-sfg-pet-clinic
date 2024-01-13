fb6ea9a50c29537725d23a016800853967b31b24
Matt Wiley <matt.wiley@8451.com>
2024-01-12 20:15:11 -0500

    Created multi-module Maven project. Closes #1.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/UdemySfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/UdemySfgPetClinicApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UdemySfgPetClinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UdemySfgPetClinicApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UdemySfgPetClinicApplication.class, args);
+  }
 
 }
