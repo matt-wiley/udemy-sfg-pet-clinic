8ce68ffbd7577e7c52eb757300ad3bca098f4893
Matt Wiley <matt.wiley@8451.com>
2024-01-14 19:46:43 -0500

    Added owners index and controller, updated request mappings. Close #14

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerContoller.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerContoller.java
@@ -3,13 +3,13 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerContoller {
 
-  @RequestMapping({"","/", "/index", "/index.html"})
-  public String listVets() {
-    return "vets/index";
+  @RequestMapping({"", "/", "/index", "/index.html"})
+  public String listOwners() {
+    return "owners/index";
   }
 
 }
